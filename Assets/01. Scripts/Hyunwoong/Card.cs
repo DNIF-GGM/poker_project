2c96ff732d402e0177021be86f0f938328dc4267
ehdbs28 <doyun5373@naver.com>
2022-09-09 02:45:37 +0900

    .

--- Assets/01. Scripts/Hyunwoong/Card.cs
+++ Assets/01. Scripts/Hyunwoong/Card.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using System;
 using System.Collections;
 
-public class Card : MonoBehaviour {
-    [SerializeField] protected Dictionary<string, int> cardInfo = new Dictionary<string, int>();
+public abstract class Card : MonoBehaviour {
+    public CardEnum _cardEnum;
+    public float _cardCost;
+    public Sprite _cardSprite;
+
+    public CardEnum SetCardEnum(int cardNum){
+        if(cardNum == 52) _cardEnum = CardEnum.Joker;
+        else{
+            _cardEnum = (CardEnum)cardNum;
+        }
+
+        return _cardEnum;
+    }
+
+    public void InitSettion(){
+        
+    }
 }
