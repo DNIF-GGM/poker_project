12df5e58da824c4da6de7d97e07027b2e6cb1146
ehdbs28 <doyun5373@naver.com>
2022-09-10 04:51:20 +0900

    주석

--- Assets/01. Scripts/Lifes/Monster.cs
+++ Assets/01. Scripts/Lifes/Monster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : PoolableMono
+public class Monster : PoolableMono //아마 나중에 추상클래스로 관리 할듯
 {
     public override void Reset()
     {
--- Assets/01. Scripts/Lifes/Unit.cs
+++ Assets/01. Scripts/Lifes/Unit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : PoolableMono
+public class Unit : PoolableMono //아마 나중에 추상클래스로 관리할듯
 {
     public override void Reset()
     {
