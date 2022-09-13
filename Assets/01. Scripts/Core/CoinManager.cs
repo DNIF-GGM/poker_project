043ccfe254db6d46aa3e37c1ed6567ee0278ba77
SEH00N <06psh0501@gmail.com>
2022-09-14 08:49:14 +0900

    코인 배팅

--- Assets/01. Scripts/Core/CoinManager.cs
+++ Assets/01. Scripts/Core/CoinManager.cs
@@ -1,22 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinManager : MonoBehaviour
 {
     public static CoinManager Instance = null;
 
     private int _currentCoin;
+    private TextMeshProUGUI bettedCoinTMP = null;
+    private TextMeshProUGUI coinInfoTMP = null;
     public int BettedCoin { get; private set; } = 0;
     public int CurrentCoin {get => _currentCoin; set => _currentCoin = value;}
 
+    private void Awake()
+    {
+        bettedCoinTMP = GameObject.Find("UICanvas/BettedCoinTMP").GetComponent<TextMeshProUGUI>();
+        coinInfoTMP = GameObject.Find("UICanvas/CoinTMP").GetComponent<TextMeshProUGUI>();
+
+        coinInfoTMP.text = "Coin : " + _currentCoin;
+        bettedCoinTMP.text = "Betted Coin : " + BettedCoin;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            CurrentCoin += 10;
+            coinInfoTMP.text = "Coin : " + _currentCoin;
+        }
+    }
+
+    public bool TryBettingCoin(int value)
+    {
+        bool returnValue = _currentCoin - value > 0;
+
+        if(returnValue) 
+        {
+            _currentCoin -= value;
+            coinInfoTMP.text = "Coin : " + _currentCoin;
+            CoinBetting(value);
+        }
+
+        return returnValue;
+    }
+
     public void PayCoin(int reduceCoin){
+        if(_currentCoin - reduceCoin <= 0) return;
+
         _currentCoin -= reduceCoin;
-        BettedCoin += reduceCoin;
-        if(_currentCoin <= 0) _currentCoin = 0;
+        CoinBetting(reduceCoin);
     }
 
     public void UpdateCoin(float multipleNum){
         _currentCoin += Mathf.RoundToInt(BettedCoin * multipleNum);
+    }
+
+    public void CoinBetting(int value)
+    {
+        BettedCoin += value;
+        bettedCoinTMP.text = "Betted Coin : " + BettedCoin;
     }
 }
