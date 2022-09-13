795ef04319be6c66a754e28a64ce1c5581bb39a9
SEH00N <06psh0501@gmail.com>
2022-09-13 09:00:30 +0900

    field 세팅

--- Assets/01. Scripts/Lifes/UnitBase.cs
+++ Assets/01. Scripts/Lifes/UnitBase.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class UnitBase : PoolableMono
 {
+    [field : SerializeField]
     public AgnetDataSO _Data { get; private set; } //SO
     public AgentState _CurState { get; set; } //현재 상태 (Flag 달아놓음 Flag 연산으로)
     public float _UnitHp { get; set; } = 0f; //현재 체력
@@ -73,7 +74,7 @@ public class UnitBase : PoolableMono
         StopAllCoroutines(); //죽었을 떄 모든 코루틴 삭제
     }
 
-    private AgentState GetState()
+    protected virtual AgentState GetState()
     {
         AgentState returnState = AgentState.Idle; //default 값 Idle 세팅
 
@@ -88,7 +89,7 @@ public class UnitBase : PoolableMono
         return returnState; //설정한 AgentState값 리턴
     }
 
-    private IEnumerator Cycle()
+    protected virtual IEnumerator Cycle()
     {
         while(!_CurState.HasFlag(AgentState.Die)) //죽으면 와이문 깨져서 die 유니티 이벤트 실행
         {
