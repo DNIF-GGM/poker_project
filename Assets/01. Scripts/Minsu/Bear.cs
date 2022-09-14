2fd6ee5d3791b0476a402d4ce696cd5444d8eac3
hueam <zminsu5@gmail.com>
2022-09-15 01:01:29 +0900

    Bear

---
+++ Assets/01. Scripts/Minsu/Bear.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonsterBase
+{
+    public override void BasicAttack()
+    {
+        base.BasicAttack();
+    }
+}
