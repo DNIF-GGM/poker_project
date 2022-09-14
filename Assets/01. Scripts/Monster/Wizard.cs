2bc0c4ac16d3136138fdf97978431303b32ebb3b
SEH00N <06psh0501@gmail.com>
2022-09-15 01:17:41 +0900

    Wizard

---
+++ Assets/01. Scripts/Monster/Wizard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonsterBase
+{
+    public override void BasicAttack()
+    {
+        base.BasicAttack();
+        //이펙트 넣어야댐!
+    }
+}
