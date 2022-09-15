6cb192fd9c027541ccc4432c6d71263cbaa1396b
ehdbs28 <doyun5373@naver.com>
2022-09-15 22:23:13 +0900

    test

---
+++ Assets/01. Scripts/Monster/FarAwayDragon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarAwayDragon : MonsterBase
+{
+    public override void BasicAttack()
+    {
+        base.BasicAttack();
+    }
+}
