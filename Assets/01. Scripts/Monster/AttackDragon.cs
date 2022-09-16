26bd83c024959577dd7514c32e90f43828cc0731
hueam <zminsu5@gmail.com>
2022-09-16 19:43:17 +0900

    .

--- Assets/01. Scripts/Monster/AttackDragon.cs
+++ Assets/01. Scripts/Monster/AttackDragon.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class AttackDragon : MonsterBase
 {
+
+    Transform mouse;
     AudioSource _as;
     private void Start()
     {
         _as = GetComponent<AudioSource>();
-    Transform mouse;
-    private void Start()
-    {
         mouse = transform.Find("Mouse").GetComponent<Transform>();
-
     }
     public override void BasicAttack()
     {
+        SetTarget(out Transform target, 1 << 6);
         FireBall fireBall = PoolManager.Instance.Pop("FireBall") as FireBall;
         fireBall.transform.position = mouse.position;
-        fireBall.Init(_target);
+        fireBall.Init(target);
         base.BasicAttack();
         //이펙트 넣어야댐!
         AudioManager.Instance.PlayEffectAudio("DragonAttack", _as);
