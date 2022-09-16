daf98d0641021188ccc53a469697252c01738f3e
gusdnd01 <cestop42@naver.com>
2022-09-16 19:28:17 +0900

    사운드 추가 및 몬스터 사운드

--- Assets/01. Scripts/Monster/FarAwayDragon.cs
+++ Assets/01. Scripts/Monster/FarAwayDragon.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class FarAwayDragon : MonsterBase
 {
+    AudioSource _as;
+    private void Start()
+    {
+        _as = GetComponent<AudioSource>();
+    }
     public override void BasicAttack()
     {
         base.BasicAttack();
+        //이펙트 넣어야댐!
+        AudioManager.Instance.PlayEffectAudio("DragonAttack", _as);
+        _as.Play();
     }
 }
--- Assets/01. Scripts/Monster/Spider.cs
+++ Assets/01. Scripts/Monster/Spider.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Spider : MonsterBase
 {
+    AudioSource _as;
+    private void Start()
+    {
+        _as = GetComponent<AudioSource>();
+    }
     public override void BasicAttack()
     {
         base.BasicAttack();
         //이펙트 넣어야댐!
+        AudioManager.Instance.PlayEffectAudio("Swing", _as);
+        _as.Play();
     }
 }
