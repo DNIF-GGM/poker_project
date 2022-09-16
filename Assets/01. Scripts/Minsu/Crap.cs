167c58800fc1fc06032d1c7ac4e2b1d1ccd3099e
hueam <zminsu5@gmail.com>
2022-09-16 13:51:53 +0900

    Bear

---
+++ Assets/01. Scripts/Minsu/Crap.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crap : MonsterBase
+{
+    public override void BasicAttack()
+    {
+        base.BasicAttack();
+    }
+}
