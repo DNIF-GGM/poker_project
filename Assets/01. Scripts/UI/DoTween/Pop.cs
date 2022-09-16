4129723fe98c2f408ae8cbbfdbad8daba13a3f68
ehdbs28 <doyun5373@naver.com>
2022-09-16 15:49:24 +0900

    UI작업

--- Assets/01. Scripts/UI/DoTween/Pop.cs
+++ Assets/01. Scripts/UI/DoTween/Pop.cs
@@ -2,18 +2,7 @@ using UnityEngine;
 
 public class Pop : MonoBehaviour
 {
-    [SerializeField] private RectTransform rect;
-    [SerializeField] private float duration;
-
-    public void DoPopUp()
-    {
-        //닷트윈으로 수정
-        rect.gameObject.SetActive(true);
-    }
-
-    public void DoPopDown()
-    {
-        //닷트윈으로 수정
-        rect.gameObject.SetActive(false);
+    public void Exit(){
+        Application.Quit();
     }
 }
